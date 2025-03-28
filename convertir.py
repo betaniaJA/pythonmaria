176d51b9323c860f877a52290c4712cc2eddb6ac
betaniaJA <betania.ja17@gmail.com>
2025-03-28 19:57:06 -0400

    Add files via upload

---
+++ convertir.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+# Load the YOLO11 model
+model = YOLO("best.pt")
+
+
+
+# Export the model to TFLite format
+model.export(format="tflite")  # creates 'yolo11n_float32.tflite'
+
+# Load the exported TFLite model
+tflite_model = YOLO("yolo11n_float32.tflite")
